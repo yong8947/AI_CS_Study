f4f54e2b82cfb5a17386b6ec7671a83c0ae54c8b
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-08-24 18:23:09 +0900

    새로운 풀이 추가
    ㅈㄱㄴ

--- Python/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ Python/프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,3 +1,27 @@
 str = input()
 
 print(str.swapcase()) #.swapcase = 대소문자 반전 메서드
+
+
+"""
+---- 또 다른 유용한 풀이들 ----
+
+
+1. 알고리즘 좋은 풀이
+print(''.join(x.upper() if x == x.lower() else x.lower() for x in input()))
+
+
+2. 정석
+str = input()
+a = ''
+
+for s in str :
+    if(s.isupper()) :
+        a = a + s.lower()
+    else : 
+        a = a + s.upper()
+
+print(a)
+
+
+"""
