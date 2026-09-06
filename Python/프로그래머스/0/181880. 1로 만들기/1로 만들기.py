1671417861bee0ab167dd3d8eac0d86015a44ce6
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-06 18:53:40 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    cnt = 0
+    for i in num_list:
+        while i>1:
+            if i%2:
+                i = (i-1)//2
+                cnt+=1
+            else:
+                i = i//2
+                cnt+=1
+    return cnt
