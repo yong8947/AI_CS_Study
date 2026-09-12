8f2d8027a5bbc0c56a8db8e60fc7c70ef4ff320b
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-12 15:20:53 +0900

    more easy solution sharing
    more easy solution sharing

--- Python/프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ Python/프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -5,3 +5,11 @@ def solution(arr, delete_list):
         if num in delete_list:
             arr2.remove((i,num))
     return [sorted(arr2)[n][1] for n in range(len(arr2))]
+
+""" 
+
+훨씬 간단한 풀이 ;;
+def solution(arr, delete_list):
+    return [x for x in arr if x not in delete_list]
+    
+"""
