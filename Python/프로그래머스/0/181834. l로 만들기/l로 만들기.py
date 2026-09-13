180df156e55e011ede2b2f764dc45b4756b36c3b
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-13 18:38:10 +0900

    [level 0] Title: l로 만들기, Time: 4.46 ms, Memory: 12.4 MB -BaekjoonHub

--- Python/프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ Python/프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,9 +1,3 @@
 def solution(myString):
-    alpha_before_l = ['a','b','c','d','e','f','g','h','i','j','k']
-    new_str = ''
-    for i in myString:
-        if i in alpha_before_l:
-            new_str += 'l'
-        else: new_str += i
-    return new_str
-    
+    answer = ['l' if x < 'l' else x for x in myString]
+    return "".join(answer)
