4d80f9c46475d23cf13188034a18d6c7d640f80f
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-13 18:47:52 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.05 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ Python/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -5,4 +5,12 @@ def solution(n):
         arr2[i] = 1
         arr.append(arr2)
     return arr
-    
+
+"""
+
+def solution(n):
+    answer=[[0]*n for i in range(n)]
+    for i in range(n): answer[i][i]=1
+    return answer
+
+"""
