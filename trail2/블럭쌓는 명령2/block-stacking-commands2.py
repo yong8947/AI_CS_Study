31017b35b6aeeb1f120d4f9b7c958b00d8a2b80b
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-15 15:41:43 +0900

    [Passed] 블럭쌓는 명령2 (58ms, 16MB)

--- trail2/블럭쌓는 명령2/block-stacking-commands2.py
+++ trail2/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,7 +3,7 @@ commands = [tuple(map(int, input().split())) for _ in range(k)]
 
 # Please write your code here.
 
-arr = [0]*n
+arr = [0]*(n+1) 
 for a,b in commands:
     for i in range(a,b+1):
         arr[i] += 1
