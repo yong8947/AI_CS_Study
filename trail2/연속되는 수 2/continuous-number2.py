606c200fa8d828ca39e3a626b9afaad4621a1ac8
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-18 15:19:10 +0900

    [Runtime Error] 연속되는 수 2 (78ms, 19MB)

--- trail2/연속되는 수 2/continuous-number2.py
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -2,8 +2,12 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-cnt = 0
+arr2 = []
+cnt = 1
 for i in range(n):
-    if i==0 or arr[i] == arr[i-1]:
-        cnt += 1
-print(cnt)
+    if i==0 or arr[i]==arr[i-1]:
+        cnt+=1
+    else:
+        arr2.append(cnt)
+        cnt = 1
+print(max(arr2))
