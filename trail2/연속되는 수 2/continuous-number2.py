40cac4f01c96974c1db34a67b6db79c4580516d7
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-18 15:28:23 +0900

    [Passed] 연속되는 수 2 (76ms, 17MB)

--- trail2/연속되는 수 2/continuous-number2.py
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -4,13 +4,11 @@ arr = [int(input()) for _ in range(n)]
 # Please write your code here.
 arr2 = []
 cnt = 1
-for i in range(n):
-    if i == 0:
-        continue
+for i in range(1,n):
+    if arr[i] == arr[i-1]:
+        cnt+=1
     else:
-        if arr[i] == arr[i-1]:
-            cnt+=1
-        else:
-            arr2.append(cnt)
-            cnt = 1
+        arr2.append(cnt)
+        cnt = 1
+arr2.append(cnt)
 print(max(arr2))
