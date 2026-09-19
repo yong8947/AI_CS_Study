634ab96f670453106bb77a693c25874642e391c6
HyunWoo <74997675+yong8947@users.noreply.github.com>
2026-09-19 13:17:45 +0900

    [Passed] 벌금은 누구에게 (51ms, 16MB)

--- trail2/벌금은 누구에게/who-will-pay.py
+++ trail2/벌금은 누구에게/who-will-pay.py
@@ -2,11 +2,11 @@ N, M, K = map(int, input().split())
 student = [int(input()) for _ in range(M)]
 
 # Please write your code here.
-arr = []
+arr = [0] * (N+1)
+ans = -1
 for i in student:
-    arr.append(i)
-    if arr.count(i) >= K:
-        print(i)
+    arr[i] += 1
+    if arr[i] >= K:
+        ans = i
         break
-if not arr:
-    print(-1)
+print(ans)
